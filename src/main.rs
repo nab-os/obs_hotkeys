d61464a395cd210a58a71278f797395631d04211
Nabos <nab-os@users.noreply.github.com>
2023-01-06 11:03:28 +0100

    Added obs connexion parameters configuration

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,52 @@
+use std::fs;
+use std::{default::Default, path::Path};
+
 use anyhow::Result;
 use obws::{requests::hotkeys::KeyModifiers, Client};
 
 use clap::{Args, Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+
+//===== Config =====//
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct ConfigFile {
+    #[serde(default)]
+    address: Option<String>,
+
+    #[serde(default)]
+    port: Option<u16>,
+
+    #[serde(default)]
+    password: Option<String>,
+}
+
+#[derive(Debug)]
+struct Config {
+    address: String,
+    port: u16,
+    password: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            address: String::from("localhost"),
+            port: 4455,
+            password: String::from("password"),
+        }
+    }
+}
+
+impl From<ConfigFile> for Config {
+    fn from(config: ConfigFile) -> Self {
+        let default = Config::default();
+        Config {
+            address: config.address.unwrap_or(default.address),
+            port: config.port.unwrap_or(default.port),
+            password: config.password.unwrap_or(default.password),
+        }
+    }
+}
 
 //===== Arg Parser =====//
 
@@ -15,6 +60,22 @@ use clap::{Args, Parser, Subcommand};
 struct Cli {
     #[clap(subcommand)]
     action: Action,
+
+    ///YAML Configuration file to load the parameters from
+    #[arg(short, long, default_value = "~/.config/obs_hotkeys/config.yaml")]
+    config_file: String,
+
+    ///OBS host address to connect to
+    #[arg(short, long)]
+    address: Option<String>,
+
+    ///OBS host port to connect to
+    #[arg(short, long)]
+    port: Option<u16>,
+
+    ///OBS host password to connect with
+    #[arg(long)]
+    password: Option<String>,
 }
 
 #[derive(Subcommand, Debug)]
@@ -60,7 +121,35 @@ struct SequenceAction {
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Cli::parse();
-    let client = Client::connect("localhost", 4455, Some("tTCuXSshGJzfZQxg")).await?;
+
+    //Default configuration
+    let config_path = shellexpand::tilde(&args.config_file);
+    let mut config: Config = if Path::new(&config_path.to_string()).exists() {
+        let config_txt = fs::read_to_string(&config_path.to_string())
+            .expect("Unable to read configuration file");
+        let config_file: ConfigFile =
+            serde_yaml::from_str(&config_txt).expect("Could not parse configuration file");
+        config_file.into()
+    } else {
+        Config::default()
+    };
+
+    //Configuration overrides
+    if let Some(address) = args.address {
+        config.address = address;
+    }
+    if let Some(port) = args.port {
+        config.port = port;
+    }
+    if let Some(password) = args.password {
+        config.password = password;
+    }
+
+    println!(
+        "Connecting to '{}' on port '{}' with password '{}'",
+        config.address, config.port, config.password
+    );
+    let client = Client::connect(config.address, config.port, Some(config.password)).await?;
 
     match args.action {
         Action::List => {
